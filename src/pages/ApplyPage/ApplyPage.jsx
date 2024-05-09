87493ffb3d619448eb4423eb77843e8e5dac34b5
Richard Mundy <mundy.richard@icloud.com>
2024-05-09 03:22:31 -0700

    add switch

--- src/pages/ApplyPage/ApplyPage.jsx
+++ src/pages/ApplyPage/ApplyPage.jsx
@@ -1,12 +1,30 @@
 import { useState } from "react";
 import CoderApplication from "../../components/CoderApplication/CoderApplication";
 import "./ApplyPage.css";
+import { Switch, FormControlLabel } from "@mui/material";
 
 export default function ApplyPage() {
+  const [coder, setCoder] = useState(true);
+
+  function handleSwitchChange() {
+    setCoder(!coder);
+  }
+
   return (
     <div className="apply-container">
       <h1>Apply Page</h1>
-      <CoderApplication />
+      <FormControlLabel
+        control={<Switch checked={coder} onChange={handleSwitchChange} />}
+        label="Label"
+        style={{ float: "right" }}
+      />
+      {coder ? (
+        <CoderApplication />
+      ) : (
+        <>
+          <h2>Organizations</h2>
+        </>
+      )}
     </div>
   );
 }
