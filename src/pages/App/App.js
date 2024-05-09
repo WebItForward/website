c7f6e074eb9ed236abe0b0155022ba7a0c4f0f29
Richard Mundy <mundy.richard@icloud.com>
2024-05-08 20:34:18 -0700

    refactor App.js

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,5 +1,10 @@
 import { Route, Routes } from "react-router-dom";
 import NavBar from "../../components/NavBar/NavBar";
+import HomePage from "../HomePage/HomePage";
+import AboutPage from "../AboutPage/AboutPage";
+import ProjectPage from "../ProjectsPage/ProjectPage";
+import ContactPage from "../ContactPage/ContactPage";
+import ApplyPage from "../ApplyPage/ApplyPage";
 import "./App.css";
 
 function App() {
@@ -7,7 +12,11 @@ function App() {
     <div className="App">
       <NavBar />
       <Routes>
-        <Route path="/" />
+        <Route path="/" element={<HomePage />} />
+        <Route path="/about" element={<AboutPage />} />
+        <Route path="/projects" element={<ProjectPage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/apply" element={<ApplyPage />} />
       </Routes>
     </div>
   );
